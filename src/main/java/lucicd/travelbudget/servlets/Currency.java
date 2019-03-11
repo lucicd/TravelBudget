452b1ad09cefdca06f4aa6bd72d163faceb1725d
Drazen <lucicd@hotmail.com>
2019-03-11 12:23:25 +0400

    First servlet, back to Bootstrap

---
+++ src/main/java/lucicd/travelbudget/servlets/Currency.java
@@ -0,0 +1,39 @@
+package lucicd.travelbudget.servlets;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "Currency", urlPatterns = {"/currencies"})
+public class Currency extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        RequestDispatcher dispatcher = request.getRequestDispatcher(
+                    "/WEB-INF/currencies/list.jsp");
+            dispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Travel Budget Home Page";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/home/home.jsp
@@ -0,0 +1,4 @@
+<jsp:include page="../shared/template.jsp">
+    <jsp:param name="content" value="../home/home_content"/>
+    <jsp:param name="title" value="Travel Budget Home Page"/>
+</jsp:include>
